1604ae2e0801bee8077f462a7770b58d0f1a3187
Andrea Flores <andrea94flores@gmail.com>
2019-06-11 17:22:43 -0500

    adds views

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -9,3 +10,10 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(max_length=1)
